3fdf380a2fab023181fa097208583a2886ce5edc
bhm530@myy.haaga-helia.fi <bhm530@myy.haaga-helia.fi>
2025-03-24 12:49:32 +0200

    ok

--- src/main/java/fi/bookstore/bookstore/security/UserDetailServiceImpl.java
+++ src/main/java/fi/bookstore/bookstore/security/UserDetailServiceImpl.java
@@ -6,9 +6,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Service;
 
-@Service
+
 public class UserDetailServiceImpl implements UserDetailsService {
     
     private final UserRepository userRepository;
